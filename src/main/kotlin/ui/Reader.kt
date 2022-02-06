1499e9ac6f9d01cff9b61349b81bc178be81fd2a
Daniel Imre Pethes <16624998+Dragoner7@users.noreply.github.com>
2022-02-06 21:00:27 +0100

    A version with buggy preloading

--- src/main/kotlin/ui/Reader.kt
+++ src/main/kotlin/ui/Reader.kt
@@ -7,8 +7,15 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import java.net.URL
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.math.min
+
+data class Page(val number : Int, val url : String, var isDownloading : Boolean, var img : ImageBitmap?){
+    val mutex = Mutex()
+}
 
 class Reader(val chapter: Chapter, val onStateChange : (ReaderState)->Unit) {
     private val PRELOAD_PAGES = 3
@@ -17,23 +24,36 @@ class Reader(val chapter: Chapter, val onStateChange : (ReaderState)->Unit) {
     private var ready : Boolean = false
     init {
         onStateChange(ReaderState(0, 0, ready, 0.0f, null))
-        chapterLoad(0, PRELOAD_PAGES)
+        chapterPreload(0, PRELOAD_PAGES)
     }
 
-    private val downloadedPages = mutableListOf<ImageBitmap>()
+    private val pages : List<Page> = chapter.urls.mapIndexed{i, url-> Page(i, url, false, null)}
+
+    private suspend fun downloadFuturePages(offset : Int, num : Int, afterEachDownload: (Int)->Unit){
+        for (i in offset until min(offset + num, numOfPages)) {
+            val page = pages[i]
+            page.mutex.withLock {
+                if(!page.isDownloading && page.img == null) {
+                    pages[i].isDownloading = true
+                    val img = URL(pages[i].url).openStream().buffered().use(::loadImageBitmap)
+                    pages[i].img = img
+                    pages[i].isDownloading = false
+                    afterEachDownload(i)
+                }
+            }
+        }
+    }
 
     @OptIn(DelicateCoroutinesApi::class)
-    fun chapterLoad(offset : Int, num : Int) {
+    fun chapterPreload(offset : Int, num : Int) {
         GlobalScope.launch(Dispatchers.IO) {
             try{
-                for (i in offset until min(offset + num, numOfPages)) {
-                    val img = URL(chapter.urls[i]).openStream().buffered().use(::loadImageBitmap)
-                    downloadedPages.add(img)
-                    val progress = i.toFloat() / num.toFloat()
+                downloadFuturePages(offset, num){
+                    val progress = it.toFloat() / num.toFloat()
                     onStateChange(ReaderState(0, 0, ready, progress, null))
                 }
                 ready = true
-                onStateChange(ReaderState(currentPageNumber, numOfPages, ready, 1.0f, downloadedPages[offset]))
+                onStateChange(ReaderState(currentPageNumber, numOfPages, ready, 1.0f, pages[currentPageNumber].img))
             } catch (e :Exception){
                 e.printStackTrace()
             }
@@ -43,13 +63,23 @@ class Reader(val chapter: Chapter, val onStateChange : (ReaderState)->Unit) {
     fun notifyStateChange(readerState: ReaderState){
         if(readerState.currentPageNumber != currentPageNumber){
             currentPageNumber = readerState.currentPageNumber
-            if(readerState.currentPageNumber >= downloadedPages.size){
+            if(pages[currentPageNumber].img == null){
                 ready = false
                 onStateChange(ReaderState(readerState.currentPageNumber, numOfPages, ready, 0.0f, null))
-                chapterLoad(readerState.currentPageNumber, PRELOAD_PAGES)
+                chapterPreload(readerState.currentPageNumber, PRELOAD_PAGES)
             }else{
-                onStateChange(ReaderState(currentPageNumber, numOfPages, ready, 1.0f, downloadedPages[currentPageNumber]))
+                onStateChange(ReaderState(currentPageNumber, numOfPages, ready, 1.0f, pages[currentPageNumber].img))
+                futurePreload()
             }
+        }
+    }
+
+    fun futurePreload(){
+        val end = currentPageNumber + PRELOAD_PAGES
+        val start = pages.indexOfFirst { p->p.img == null }
+        val difference = end - start
+        if(difference > 0){
+            chapterPreload(start, difference)
         }
     }
 
