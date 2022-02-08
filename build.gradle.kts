d479b2a1bbfc27ae8b1f649876ad6f003e152db0
Daniel Imre Pethes <16624998+Dragoner7@users.noreply.github.com>
2022-02-08 18:44:16 +0100

    Update kotlin codegen dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation(compose.desktop.currentOs)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
-    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
 }
 
